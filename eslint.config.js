5a7ce998a81e69d7edcdcaab16d10c2040e06c95
Kristoffer Kirkerud <kirkerudk@gmail.com>
2024-09-17 23:12:15 +0200

    chore: adjust more lint issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import { includeIgnoreFile, fixupConfigRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import xoSpaceBrowser from 'eslint-config-xo/space/browser'
+import {includeIgnoreFile, fixupConfigRules} from '@eslint/compat';
+import {FlatCompat} from '@eslint/eslintrc';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import xoSpaceBrowser from 'eslint-config-xo/space/browser';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const gitignorePath = path.resolve(__dirname, '.gitignore')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
-const flatCompat = new FlatCompat()
+const flatCompat = new FlatCompat();
 
 export default [
-    includeIgnoreFile(gitignorePath),
-    ...xoSpaceBrowser,
-    ...fixupConfigRules(flatCompat.extends('eslint-config-xo-react/space')),
-]
+  includeIgnoreFile(gitignorePath),
+  ...xoSpaceBrowser,
+  ...fixupConfigRules(flatCompat.extends('eslint-config-xo-react/space')),
+];
