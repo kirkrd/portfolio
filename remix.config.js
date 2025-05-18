239168dffc3346c08eaa021ee55fcd2c5974ee89
Kristoffer Kirkerud <kirkerudk@gmail.com>
2025-05-18 14:17:52 +0000

    chore: remove old boilerplate from remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,7 @@
-/** @type {import('@remix-run/dev').AppConfig} */
 const config = {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   postcss: true,
-  // AppDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
 };
 
 export default config;
